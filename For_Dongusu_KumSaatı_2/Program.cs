e3a9979678bcd567ac614e7a83c33622cbb0996f
Muharrem Rahat <atomtr42@gmail.com>
2022-01-06 07:45:13 +0300

    Proje dosyası ekle.

---
+++ For_Dongusu_KumSaatı_2/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Dongusu_KumSaatı_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, i, j, k, x, y, z;
+            Console.Write("Lütfen bir sayı giriniz   : ");
+            sayi = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine();
+            for (i = 1; i <= sayi; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (k = i; k <= sayi; k++)
+                {
+                    Console.Write(i);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            for (x = 1; x <= sayi; x++)
+            {
+                for (y = x; y <= sayi; y++)
+                {
+                    Console.Write(" ");
+                }
+                for (z = 1; z <= x; z++)
+                {
+                    Console.Write(x);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
